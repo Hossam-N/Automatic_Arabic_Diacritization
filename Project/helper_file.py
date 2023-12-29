a9f4fa596473bd760c7dbb1980f1102ae945a5bf
Abdulhameed Osama <meedosama2001@gmail.com>
2023-12-29 19:50:32 +0200

    Clean

--- Project/helper_file.py
+++ Project/helper_file.py
@@ -1,4 +1,5 @@
 import pickle as pkl
+import re
 def read_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         return f.read()
@@ -15,3 +16,17 @@ def write_pickle(file_path, content):
     with open(file_path, 'wb') as f:
         pkl.dump(content, f)
         
+def clean_dataset(dataset):
+    # This function is used to clean an arabic dataset using regex
+    
+    # Removing diacritics
+    # dataset = re.sub('[^ء-ي ]', '', dataset)
+    
+    # keeping diacritics
+    dataset = re.sub(r'[^؀-ۿ ]', '', dataset)
+    # remove brackets and other symbols
+    # dataset = re.sub(r'[^\u0621-\u063A\u0641-\u064A\s]',' ', dataset)
+    # Remove extra spaces
+    dataset = re.sub(r'\s+', ' ', dataset)
+    return dataset
+    
